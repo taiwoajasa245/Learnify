b29c22bfebb3c6a8b1e285f467b146ea859ab285
Ajasa Taiwo <ajasataiwo45@gmail.com>
2023-10-16 11:39:11 +0100

    initial commit

--- script.js
+++ script.js
@@ -3,41 +3,23 @@ var moreText = document.querySelectorAll("#more");
 var btnText = document.querySelectorAll("#btn-more");
 
 
-// Select all buttons with the class "my-button"
-const buttons = document.querySelectorAll(".my-button");
 
 // Define the function you want to run when a button is clicked
 function buttonClickHandler(event) {
-  // You can access the clicked button using "event.target"
-  const clickedButton = event.target;
-  // Add your code here to handle the button click
-  console.log(`Button "${clickedButton.textContent}" was clicked.`);
+//   // You can access the clicked button using "event.target"
+//   const clickedButton = event.target;
+//   // Add your code here to handle the button click
+//   console.log(`Button "${clickedButton.textContent}" was clicked.`);
+  myFunction(); 
 }
 
 // Loop through the selected buttons and attach the click event listener
 btnText.forEach((btnText) => {
     btnText.addEventListener("click",  () => { 
-        myFunction(); 
         buttonClickHandler(); 
     });
 
-
-// Define the function you want to run when a button is clicked
-function buttonClickHandler(event) {
-  // You can access the clicked button using "event.target"
-  const clickedButton = event.target;
-  // Add your code here to handle the button click
-  console.log(`Button "${clickedButton.textContent}" was clicked.`);
-}
-
-// Loop through the selected buttons and attach the click event listener
-moreText.forEach((moreText) => {
-    moreText.addEventListener("click",  () => { 
-        myFunction(); 
-        buttonClickHandler(); 
-    });
-});
-});
+})
 
 // btnText.addEventListener('click', () =>  { myFunction(); })
 
@@ -55,4 +37,4 @@ function myFunction() {
       btnText.innerHTML = "Read less";
       moreText.style.display = "inline";
     }
-  }
+}
